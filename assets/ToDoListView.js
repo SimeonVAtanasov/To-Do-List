4e47008257829d647262419c0e3a510cff4c52f6
Simeon Atanasov <simoatansov76@gmail.com>
2021-02-21 17:41:44 +0200

    Refactored code

--- assets/ToDoListView.js
+++ assets/ToDoListView.js
@@ -1,33 +1,12 @@
-function styleNewTaks(message, newBox, newTask) {
-    message.style.display = 'none';
-
-    newBox.style.display = 'block'
-
-    newBox.style.textAlign = 'initial';
-    newBox.style.fontSize = "30px";
-
-};
-
-function hideElement(element) {
-    element.style.display = "none"
-}
-
-function showElement(element, property) {
-    if (property) {
-        element.style.display = property;
-        return;
-    }
-
-    element.style.display = "block";
-}
-
 function fade(element) {
     let op = 1;  // initial opacity
     let timer = setInterval(function () {
+
         if (op <= 0.1) {
             clearInterval(timer);
             element.style.display = 'none';
         }
+
         element.style.opacity = op;
         element.style.filter = 'alpha(opacity=' + op * 100 + ")";
         op -= op * 0.1;
@@ -36,11 +15,13 @@ function fade(element) {
 
 function unfade(element, displayProperty, time = 10) {
     let op = 0.1;  // initial opacity
+
     if (displayProperty) {
         element.style.display = displayProperty;
     } else {
         element.style.display = 'block';
     }
+
     let timer = setInterval(function () {
         if (op >= 1) {
             clearInterval(timer);
